aff0cbeab71504607c8954cf241d422a022ecbc1
Christian Ost <christian@christianost.de>
2018-12-16 22:15:47 +0100

    improve setup script and documentation

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+# publish module to the local yalc-"registry"
 node_modules/.bin/yalc publish
 
+# change working directory to Example
 cd Example
+
+# install dependencies
+yarn install --pure-lockfile
+
+# use local library from the yalc-"registry"
 ../node_modules/.bin/yalc link react-native-bluetooth-state
+
+# link binaries in case they aren’t linked yet
 react-native link react-native-bluetooth-state
