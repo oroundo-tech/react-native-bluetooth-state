203a1a1584641ca68ed9b04c3623bff55f13d03a
Christian Ost <christian@christianost.de>
2018-12-20 21:38:17 +0100

    improve setup script

--- dev-setup.sh
+++ dev-setup.sh
@@ -6,9 +6,11 @@ node_modules/.bin/yalc publish
 # change working directory to Example
 cd Example
 
+# unlink binaries in case they are to prevent libraries not being linked correctly
+node node_modules/react-native/local-cli/cli.js unlink react-native-bluetooth-state
+
 # use local library from the yalc-"registry"
 ../node_modules/.bin/yalc link react-native-bluetooth-state
 
 # link binaries in case they aren’t linked yet
-# react-native link react-native-bluetooth-state
 node node_modules/react-native/local-cli/cli.js link react-native-bluetooth-state
